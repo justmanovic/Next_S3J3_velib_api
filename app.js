3609d0f31894cad9e2b924b2a3d0f6e590ae5b54
jonathan justman <jojo.justman@gmail.com>
2021-10-12 18:46:06 +0200

    improvements

--- app.js
+++ app.js
@@ -22,23 +22,19 @@ const getVelibStations = async () => {
   // container.innerHTML = ''
   const response = await fetch("https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&rows=100&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes")
   const myJson = await response.json();
-
-
-
   // myJson.records.forEach(station => showVelibStation(container, station.fields.name, station.fields.mechanical, station.fields.ebike))
 
   myJson.records.forEach(station => {
     coords = [station.fields.coordonnees_geo[0], station.fields.coordonnees_geo[1]]
     let marker = L.marker(coords).addTo(mymap);
-    html = `<ul><li><b>Nom de la station : </b>${station.fields.name}</li><li><b>Nombre de vélos mécaniques: </b>${station.fields.mechanical}</li><li><b>Nombre de vélos électriques: </b>${station.fields.ebike}</li></ul>`
+    html = `<ul>
+              <li><b>Nom de la station : </b>${station.fields.name}</li>
+              <li><b>Nombre de vélos mécaniques: </b>${station.fields.mechanical}</li>
+              <li><b>Nombre de vélos électriques: </b>${station.fields.ebike}</li>
+            </ul>`
     marker.bindPopup(html).openPopup();
   })
-
 }
-
-
-
-
 
 var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors',
@@ -49,5 +45,5 @@ mymap.addLayer(osmLayer);
 
 getVelibStations()
 
-setInterval(getVelibStations, 60000)
+setInterval(getVelibStations, 5000)
 
